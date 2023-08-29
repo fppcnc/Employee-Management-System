7df538b92977c9366df7a60fc4c291456836d2d2
Fil <concatofilippo94@gmail.com>
2023-08-29 10:10:57 +0200

    deutsch version

--- sql/data.sql
+++ sql/data.sql
@@ -22,8 +22,24 @@ CREATE TABLE employee
     foreign key (departmentId) references departments (id)
 );
 
-insert into departments
-values (NULL, 'admin');
 
-insert into employee
-values (NULL, 'AAA', 'AAA', 1);
+INSERT INTO departments (name)
+VALUES ('HR'),
+       ('IT'),
+       ('Sales');
+
+
+INSERT INTO employee (firstName, lastName, departmentId)
+VALUES ('John', 'Doe', 1),
+       ('Jane', 'Smith', 2),
+       ('Michael', 'Johnson', 3),
+       ('Emily', 'Wilson', 2),
+       ('David', 'Brown', 1),
+       ('Sara', 'Garcia', 3),
+       ('Daniel', 'Lee', 2),
+       ('Maria', 'Martinez', 1),
+       ('Robert', 'Johnson', 3),
+       ('Jessica', 'Anderson', 2),
+       ('William', 'Taylor', 1),
+       ('Linda', 'Williams', 3),
+       ('Richard', 'Miller', 2);
